3cb69ec3a61c7f21fb6239df8b404d28d38b71d8
Nithya Sri <duvvurunithyasri@gmail.com>
2026-06-26 12:24:51 +0530

    Added Week1 Exercises

---
+++ Week1/Exercise1_SingletonPattern/Code/LoggerTest.java
@@ -0,0 +1,15 @@
+public class LoggerTest {
+    public static void main(String[] args){
+        Logger logger1 = Logger.getInstance();
+        logger1.log("First Message");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Second Message");
+
+        if(logger1 == logger2){
+            System.out.println("Only one logger object is created");
+        } else{
+            System.out.println("Different objects created");
+        }
+    }
+}
